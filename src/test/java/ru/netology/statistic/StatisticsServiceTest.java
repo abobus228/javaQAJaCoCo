7573a8a0712778ac67d350c884331350dcc812aa
abobus228 <sashasafronov767@gmail.com>
2025-02-05 15:59:29 +0300

    Test coverage in JaCoCo

--- src/test/java/ru/netology/statistic/StatisticsServiceTest.java
+++ src/test/java/ru/netology/statistic/StatisticsServiceTest.java
@@ -16,4 +16,18 @@ public class StatisticsServiceTest {
 
         Assertions.assertEquals(expected, actual);
     }
+
+    // Тест для 100% покрытия кода тестами
+    // Дополнительный тест по нахождению максимального значения, если первый элемент не является самым большим
+    @Test
+    void findMaxIfFirstDigitNotMax() {
+        StatisticsService service = new StatisticsService();
+
+        long[] incomesInBillions = {8, 5, 8, 4, 5, 3, 8, 6, 15, 11, 12};
+        long expected = 15;
+
+        long actual = service.findMax(incomesInBillions);
+
+        Assertions.assertEquals(expected, actual);
+    }
 }
